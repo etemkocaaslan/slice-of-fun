9698242198adb16cb6bc662bee97b876e760d118
etemkocaaslan <120686301+etemkocaaslan@users.noreply.github.com>
2023-11-22 12:12:02 -0800

    Add RawMaterial base class and Vegetable subclasses
    This commit introduces the RawMaterial class and its subclass Vegetable, along with specific vegetable types (Carrot and Tomato), under the SliceOfFun namespace. The RawMaterial class serves as an abstract base for various types of ingredients used in the pizza store. It includes a protected constructor that initializes the 'Type' of the material, ensuring it cannot be null, thus enforcing data integrity.
    
    Key features of the RawMaterial class:
    - A protected property 'Type' to denote the kind of raw material.
    - Public virtual properties 'Quantity' and 'UOM' (Unit of Measure) for inventory management.
    
    The Vegetable class is a direct subclass of RawMaterial, specifically representing vegetables with a predefined 'Type' set to "Vegetable".
    
    Further, two specific types of vegetables are introduced:
    - Carrot: Overrides the Quantity property to handle its specific quantity management.
    - Tomato: Inherits directly from Vegetable with default implementations.
    
    This structure lays the groundwork for detailed inventory management of various raw materials and ingredients. It is flexible enough to accommodate different types of materials and their unique characteristics, making it a crucial part of the store's operational management system.

---
+++ SliceOfFun/RawMaterial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SliceOfFun
+{
+    abstract class RawMaterial
+    {
+        protected RawMaterial(string? type)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+
+        protected string? Type { get; private set; }
+        public virtual decimal Quantity { get; set; }
+        public virtual string? UOM { get; }
+    }
+
+    abstract class Vegetable : RawMaterial
+    {
+        protected Vegetable() : base("Vegetable")
+        {
+        }
+    }
+
+    class Carrot : Vegetable 
+    {
+        public override decimal Quantity { get => base.Quantity; set => base.Quantity = value; }
+        public Carrot()
+        {
+
+        }
+    }
+    class Tomato : Vegetable { }
+}
