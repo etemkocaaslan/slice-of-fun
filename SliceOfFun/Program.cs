1be7b5ed18ac5d5223f67b060b4c2ef17bb386c3
etemkocaaslan <120686301+etemkocaaslan@users.noreply.github.com>
2023-11-22 12:15:03 -0800

    Test

--- SliceOfFun/Program.cs
+++ SliceOfFun/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SliceOfFun;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee etem = new Employee(
+            name: "Etem",
+            age: 27,
+            gender: "Male",
+            nationality: "Turkish",
+            isCitizen: true
+            );
+        UncleEtemPizza uncleEtemPizza = new UncleEtemPizza(new Owner(etem));
+        
+        Console.WriteLine( uncleEtemPizza );
+    }
+}
