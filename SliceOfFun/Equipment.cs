2f46c67b3615151a49d471a42db58c376f4d6d7c
etemkocaaslan <120686301+etemkocaaslan@users.noreply.github.com>
2023-11-22 12:08:04 -0800

    This commit introduces a new abstraction in the project: the Equipment class. This class serves as a base for various types of equipment that will be used in the SliceOfFun namespace. The Equipment class is abstract and includes a protected constructor that initializes the 'Name' property, which is common to all equipment.
    Additionally, this commit includes the first concrete implementation of an Equipment subclass - DoughMixer. The DoughMixer class inherits from Equipment and is initialized with a specific name ("Dough Mixer") passed to the base class constructor. This sets a precedent for future equipment classes, which can now be easily added to the system with their unique properties and behaviors.
    
    This enhancement paves the way for incorporating more complex equipment-related functionalities and details into the project, enhancing the simulation and management aspects of the pizza store scenario.

---
+++ SliceOfFun/Equipment.cs
@@ -0,0 +1,19 @@
+﻿namespace SliceOfFun
+{
+    abstract class Equipment
+    {
+        public string Name { get; private set; }
+
+        protected Equipment(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class DoughMixer : Equipment
+    {
+        public DoughMixer() : base("Dough Mixer") { }
+
+    }
+
+}
