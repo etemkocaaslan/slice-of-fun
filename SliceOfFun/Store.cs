63d457c447c1d56de9e5441b0aefa9457b22d3fb
etemkocaaslan <120686301+etemkocaaslan@users.noreply.github.com>
2023-11-22 12:14:43 -0800

    Implement Store class hierarchy for franchise and local stores
    This commit adds a significant feature to the SliceOfFun namespace by implementing a class hierarchy to manage different types of pizza stores, including both franchise and local stores. The Store class serves as an abstract base class, defining properties common to all store types, such as whether the store is a franchise, its name, location, and ownership details.
    
    Key features and changes:
    - The Store class includes properties like IsFranchiseStore, FranchiseName, CompanyName, LocationName, Owner, and FranchiseOwner. It also has a constructor that sets the IsFranchiseStore property.
    - FranchisePizzaStore, an abstract subclass of Store, specifically represents franchise stores. It initializes the CompanyName based on the subclass type and has a protected constructor for setting franchise-specific details.
    - UncleEtemPizza and PizzaBalcony classes, both inherit from FranchisePizzaStore, representing specific franchise store types. UncleEtemPizza class includes an Owner property, while PizzaBalcony class allows setting a custom LocationName.
    - LocalPizzaStore class represents non-franchise local stores, with the IsFranchiseStore property overridden to return false.
    
    This structure allows for a clear distinction between franchise and local pizza stores, enabling more tailored management and functionalities for each type. It lays a foundation for expanding the application to handle various operational aspects of different store types within the pizza business scenario.

---
+++ SliceOfFun/Store.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SliceOfFun
+{
+    abstract class Store
+    {
+        public virtual bool IsFranchiseStore { get; private set; }
+        public virtual string? FranchiseName { get; init; }
+        public virtual string? CompanyName { get; init; }
+        public virtual string? LocationName { get; init; }
+        public virtual Owner? Owner { get; set; }
+        public virtual Person? FranchiseOwner { get; set; }
+
+
+        public Store(bool isfranchisestore) => IsFranchiseStore = isfranchisestore;
+    }
+
+    abstract class FranchisePizzaStore : Store
+    {
+        protected FranchisePizzaStore(string? FranchiseName, Person? FranchiseOwner) : base(true)
+        {
+            CompanyName = GetType().Name;
+        }
+    }
+
+    class UncleEtemPizza : FranchisePizzaStore
+    {
+        public UncleEtemPizza(Owner owner) : base(FranchiseName: "SNA Pizza", null)
+        {
+            Owner = owner;
+        }
+    }
+
+    class PizzaBalcony : FranchisePizzaStore
+    {
+        public override string? LocationName { get => base.LocationName; init => base.LocationName = value; }
+
+        public PizzaBalcony() : base(FranchiseName: "SNA Pizza", null)
+        {
+        }
+    }
+    class LocalPizzaStore : Store
+    {
+        public override bool IsFranchiseStore => false;
+
+        public LocalPizzaStore(string? locationName, Person owner) : base(false)
+        {
+        }
+    }
+}
+
+
