47f05251df3ff37b5848578ef7057e7db1b8ea7f
kasla <kasla@DESKTOP-SR937O1>
2023-12-06 19:11:23 -0800

    Added `Product.cs` in `SliceOfFun` namespace, defining `Product` class with `RawMaterial` and `RawMaterialType` enums. Structured `Product` to include material and type properties, and a comprehensive list of raw materials and their types. Updated `Provider` class to incorporate these changes, enhancing product management and availability checks.

---
+++ SliceOfFun/Product.cs
@@ -0,0 +1,57 @@
+﻿namespace SliceOfFun
+{
+    public readonly struct Product
+    {
+        private readonly RawMaterial rM;
+        private readonly RawMaterialType rMType;
+
+        public Product(RawMaterial rM, RawMaterialType rMType)
+        {
+            this.rM = rM;
+            this.rMType = rMType;
+        }
+
+        public readonly RawMaterial RM => rM;
+        public readonly RawMaterialType RMType => rMType;
+        public readonly string Name { get => rMType.ToString() + "-" + rM.ToString(); }
+
+        public enum RawMaterialType
+        {
+            Vegetable,
+            Meat,
+            Sauce,
+            Beverage,
+            Oil,
+            Cheese,
+            Flour,
+            Cleaning,
+            Service
+        }
+        public enum RawMaterial
+        {
+            GroundBeef,
+            Pepperoni,
+            Chorizo,
+            ItalianSuasage,
+            Ham,
+            Capocollo,
+            PulledPork,
+            Steak,
+            Chicken,
+            Bacon,
+            GreenOlive,
+            BlackOlive,
+            Eggplant,
+            Artichoke,
+            Pineapple,
+            RedPepper,
+            GreenPepper,
+            Tomato,
+            Spinach,
+            Mushroom
+        }
+
+        public override string ToString() => Name ?? "";
+    }
+
+}
--- SliceOfFun/Provider.cs
+++ SliceOfFun/Provider.cs
@@ -1,79 +1,46 @@
-﻿namespace SliceOfFun
+﻿using System.Collections.Immutable;
+
+namespace SliceOfFun
 {
-
-    public class Product
-    {
-        public RawMaterial RM { get; set; }
-        public RawMaterialType RMType { get; set; }
-
-        public enum RawMaterialType
-        {
-            Vegetable,
-            Meat,
-            Sauce,
-            Beverage,
-            Oil,
-            Cheese,
-            Flour,
-            Cleaning,
-            Service
-        }
-        public enum RawMaterial
-        {
-            GroundBeef,
-            Pepperoni,
-            Chorizo,
-            ItalianSuasage,
-            Ham,
-            Capocollo,
-            PulledPork,
-            Steak,
-            Chicken,
-            Bacon,
-            GreenOlive,
-            BlackOlive,
-            Eggplant,
-            Artichoke,
-            Pineapple,
-            RedPepper,
-            GreenPepper,
-            Tomato,
-            Spinach,
-            Mushroom
-        }
-    }
     public class Provider
     {
         internal Dictionary<Product, decimal> Products { get; } = new Dictionary<Product, decimal>();
+        public List<Product> Catalog { get => catalog; protected set => catalog = value; }
 
-        internal readonly List<Product> Catalog = new() {
-            new Product { RM = Product.RawMaterial.GroundBeef,      RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.Pepperoni,       RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.Chorizo,         RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.ItalianSuasage,  RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.Ham,             RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.Capocollo,       RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.PulledPork,      RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.Steak,           RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.Chicken,         RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.Bacon,           RMType = Product.RawMaterialType.Meat },
-            new Product { RM = Product.RawMaterial.GreenOlive,      RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.BlackOlive,      RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.Eggplant,        RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.Artichoke,       RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.Pineapple,       RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.RedPepper,       RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.GreenPepper,     RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.Tomato,          RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.Spinach,         RMType = Product.RawMaterialType.Vegetable },
-            new Product { RM = Product.RawMaterial.Mushroom,        RMType = Product.RawMaterialType.Vegetable },
-        };
+        List<Product> catalog = new() { };
+
+        public Provider()
+        {
+            Catalog = new List<Product>(new Product[] {
+                new Product(Product.RawMaterial.GroundBeef, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.Pepperoni, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.Chorizo, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.ItalianSuasage, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.Ham, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.Capocollo, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.PulledPork, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.Steak, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.Chicken, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.Bacon, Product.RawMaterialType.Meat),
+                new Product(Product.RawMaterial.GreenOlive, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.BlackOlive, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.Eggplant, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.Artichoke, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.Pineapple, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.RedPepper, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.GreenPepper, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.Tomato, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.Spinach, Product.RawMaterialType.Vegetable),
+                new Product(Product.RawMaterial.Mushroom, Product.RawMaterialType.Vegetable)
+            });
+        }
 
         void Refill()
         {
             foreach (Product product in Catalog)
-                Products?.Add(product, 200);
+                Products.Add(product, 200);
         }
+
         public void Refill(string request)
         {
             if (!string.IsNullOrEmpty(request))
@@ -82,15 +49,11 @@
                 throw new ArgumentException($"'{nameof(request)}' cannot be null or empty.", nameof(request));
         }
 
-        void AddOrUpdateProduct(Product product, decimal quantity)
-        {
-
-        }
-
         public string IsProductAvailable(Product product, decimal quantity)
         {
             return (Products.ContainsKey(product) && Products[product] >= quantity) ? $"Provider has enough of {product.RM}" : $"Not Enough {product.RM}";
         }
+
     }
 
 }
